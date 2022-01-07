5b15a4fd52e33d02aeabf0f2a6f32189a31b5af3
Gareth Harper <gareth.harper@digital.cabinet-office.gov.uk>
2022-01-07 16:59:15 +0000

    ignore swap files and added vpc generation

--- prototype/vpc.tf
+++ prototype/vpc.tf
@@ -1,6 +1,30 @@
+locals {
+  az_names       = data.aws_availability_zones.available.names
+  current_region = data.aws_region.current.name
+  vpc_cidr       = "10.0.0.0/16"
+}
+
+
 resource "aws_vpc" "gdx_prototype" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = local.vpc_cidr
   tags = {
     Name = "gdx-prototype-${var.stack_identifier}"
   }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_region" "current" {}
+
+resource "aws_subnet" "private" {
+  count             = length(local.az_names)
+  vpc_id            = aws_vpc.gdx_prototype.id
+  availability_zone = local.az_names[count.index]
+  cidr_block        = cidrsubnet(local.vpc_cidr, 8, count.index)
+  tags = {
+    Name = "gdx-proto-${var.stack_identifier}-private-${count.index}"
+  }
+}
+
