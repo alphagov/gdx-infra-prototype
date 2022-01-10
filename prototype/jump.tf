37910355f97eadc5cfc83f26459b7bc4c59a99f6
Gareth Harper <gareth.harper@digital.cabinet-office.gov.uk>
2022-01-10 16:00:25 +0000

    now starts to create iam roles for jump box

--- prototype/jump.tf
+++ prototype/jump.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "gdx_jump_security_group" {
+resource "aws_security_group" "jump_security_group" {
   name           = "gdx-jump-security-group"
   description    = "Jump Box for developer interactions via console with kafka"
   vpc_id         = aws_vpc.gdx_prototype.id
@@ -11,4 +11,22 @@ resource "aws_security_group_rule" "jump_allow_all_egress" {
   protocol             = "-1"
   cidr_blocks          = ["0.0.0.0/0"]
   type                 = "egress"
+}
+
+data "aws_iam_policy_document" "jump_iam_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = [
+        "ec2.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "jump_iam_role" {
+  name = "gdx-jump-iam-role"
+
+  assume_role_policy = data.aws_iam_policy_document.jump_iam_policy_document.json
 }
